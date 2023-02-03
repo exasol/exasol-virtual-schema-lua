0c41f268cee9c5d1edc653e2d34c81a19f7f8ee8
Sebastian Bär <sebastian.baer@exasol.com>
2023-02-03 11:11:47 +0100

    #18: Able to read remote meta data.

--- src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
+++ src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
@@ -1,7 +1,7 @@
 package com.exasol;
 
 public final class ExasolVirtualSchemaTestConstants {
-    public static final String DOCKER_DB = "exasol/docker-db:7.1.17";
+    public static final String DOCKER_DB = "exasol/docker-db:8.18.0";
 
     private ExasolVirtualSchemaTestConstants() {
         // prevent instantiation
