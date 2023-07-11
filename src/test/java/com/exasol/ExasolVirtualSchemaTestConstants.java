1698f9121719fb798dab6bdfb9644886cc73f94d
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-07-11 15:54:55 +0200

    Test with Exasol 7.1 & 8 in CI

--- src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
+++ src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
@@ -1,11 +1,7 @@
 package com.exasol;
 
 public final class ExasolVirtualSchemaTestConstants {
-    public static final String DOCKER_DB = isCi() ? "exasol/docker-db:7.1.21" : "exasol/docker-db:8.18.1";
-
-    private static boolean isCi() {
-        return "true".equals(System.getenv("CI"));
-    }
+    public static final String DOCKER_DB = "exasol/docker-db:8.18.1";
 
     private ExasolVirtualSchemaTestConstants() {
         // prevent instantiation
