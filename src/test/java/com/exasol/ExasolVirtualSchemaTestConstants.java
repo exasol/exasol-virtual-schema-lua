1645acf03d69680568da726176284e1419e48122
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-07-11 13:40:03 +0200

    Upgrade DB versions

--- src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
+++ src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
@@ -1,7 +1,7 @@
 package com.exasol;
 
 public final class ExasolVirtualSchemaTestConstants {
-    public static final String DOCKER_DB = isCi() ? "exasol/docker-db:7.1.18" : "exasol/docker-db:8.18.0";
+    public static final String DOCKER_DB = isCi() ? "exasol/docker-db:7.1.21" : "exasol/docker-db:8.18.1";
 
     private static boolean isCi() {
         return "true".equals(System.getenv("CI"));
