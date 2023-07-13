19a0352e9a2014351d69ce709663ad1cc375f042
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-07-13 07:39:39 +0200

    Upgrade to latest Exasol version

--- src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
+++ src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
@@ -1,7 +1,7 @@
 package com.exasol;
 
 public final class ExasolVirtualSchemaTestConstants {
-    public static final String DOCKER_DB = "exasol/docker-db:8.18.1";
+    public static final String DOCKER_DB = "exasol/docker-db:8.20.0";
 
     private ExasolVirtualSchemaTestConstants() {
         // prevent instantiation
