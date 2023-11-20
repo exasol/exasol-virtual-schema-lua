1d3e5fe6f691da05e8a787b746990be95e03df34
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-11-20 16:22:02 +0100

    #48 Fix CVE-2023-4043 in test dependency `org.eclipse.parsson:parsson` (#49)

--- src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
+++ src/test/java/com/exasol/ExasolVirtualSchemaTestConstants.java
@@ -1,7 +1,7 @@
 package com.exasol;
 
 public final class ExasolVirtualSchemaTestConstants {
-    public static final String DOCKER_DB = "exasol/docker-db:8.20.0";
+    public static final String DOCKER_DB = "exasol/docker-db:8.23.1";
 
     private ExasolVirtualSchemaTestConstants() {
         // prevent instantiation
