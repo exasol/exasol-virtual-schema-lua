7cdea6ee6eaf66d2c2252ae0139818362ab6e012
Sebastian Bär <sebastian.baer@exasol.com>
2025-03-25 13:41:09 +0100

    #58: Moved bundling to a shell script for better environment control.

---
+++ tools/bundle.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script bundles the individual Lua modules into one file.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly script_dir=$(dirname "$(readlink -f "$0")")
+if [[ -z "${1+x}" ]]
+then
+    readonly base_dir=$(readlink -f "$script_dir/..")
+else
+    readonly base_dir="$1"
+fi
+
+# Source the LuaRocks paths
+eval $(luarocks --local path)
+
+readonly exit_ok=0
+readonly exit_software=2
+
+##
+# Use the `make` target of Luarocks to bundle the modules.
+#
+function create_bundle {
+    cd "$base_dir" || exit
+    luarocks --local make
+}
+
+create_bundle \
+|| exit "$exit_software"
+
+exit "$exit_ok"
+
+}
